1ed30d8aa22b664c1e15bc459bf4c37ec171a5bf
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-12-16 12:32:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
-const username = "Mango";
-console.log("Username is ", username);
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+   if (pizzas.includes(pizzaName)){
+     return onSuccess(pizzaName);
+    console.log(onSuccess)
+   }; 
+   
+    return onError(pizzaName);
+  },
+  
+}; 
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log (pizzaPalace.order("Smoked", makePizza, onOrderError))
+pizzaPalace.order("Four meats", makePizza, onOrderError);
+pizzaPalace.order("Big Mike", makePizza, onOrderError);
+pizzaPalace.order("Vienna", makePizza, onOrderError);
